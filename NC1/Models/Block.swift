d9752e0fd78e1ea5aee9056295b476a9abda516a
seolbotdae <seolbotdae@gmail.com>
2024-04-29 00:15:44 +0900

    Feat: 앱 수정을 위한 모델 구조 변경
    SwiftData를 더 원활하게 적용하기 위한 모델 구조 변경

--- NC1/Models/Block.swift
+++ NC1/Models/Block.swift
@@ -10,24 +10,24 @@ import SwiftData
 
 @Model
 final class Block {
-    var id: Int
-    // 소속 journey
-    @Relationship(inverse: \Journey.blocks)
-    var journey: Journey?
-    // 임시로 String 해둔것
-    var photo: String?
+    /// 0 부터 시작하는 Int 타입입니다. 순서를 나타냅니다.
+    typealias Order = Int
+    
+    var id: UUID
+    var journey: Journey
+    var photo: Data?
 
     var content: String
     var editedContent: String?
     
-    var isThumbnail: Bool = false
+    var order: Order
     
-    init(id: Int, journey: Journey? = nil, photo: String? = nil, content: String, editedContent: String? = nil, isThumbnail: Bool) {
+    init(id: UUID, journey: Journey, photo: Data? = nil, content: String, editedContent: String? = nil, order: Order) {
         self.id = id
         self.journey = journey
         self.photo = photo
         self.content = content
         self.editedContent = editedContent
-        self.isThumbnail = isThumbnail
+        self.order = order
     }
 }
--- NC1/Models/Journey.swift
+++ NC1/Models/Journey.swift
@@ -10,15 +10,14 @@ import SwiftData
 
 @Model
 final class Journey {
-    @Attribute(.unique)
-    var id: Int
+    /// Date 가 2024/01/01 일인 경우, 시간에 상관없이 ParsedDate는 20240101 형태를 띄는 Int 타입입니다.
+    typealias ParsedDate = Int
     
-    @Relationship
-    var blocks: [Block] = []
-    
+    @Attribute(.unique) var id: ParsedDate
+    @Relationship(deleteRule: .cascade, inverse: \Block.journey) var blocks: [Block]?
     var thumbnail: Block?
   
-    init(id: Int, blocks: [Block], thumbnail: Block? = nil) {
+    init(id: ParsedDate, blocks: [Block]? = nil, thumbnail: Block? = nil) {
         self.id = id
         self.blocks = blocks
         self.thumbnail = thumbnail
